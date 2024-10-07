2d41d6f2020fbe404c07391a203104d05e90858c
Sonwonill <tlatmsdkqk@gmail.com>
2024-10-07 17:32:40 +0900

    :art: Fix : "병합 후 동작 되게 코드 수정 및 BP 수정"
    병합 후 동작 되게 코드 수정 및 BP 수정

--- Source/Y2S3_MISINJEON/Public/MisinjeonPlayerController.h
+++ Source/Y2S3_MISINJEON/Public/MisinjeonPlayerController.h
@@ -10,12 +10,7 @@ class Y2S3_MISINJEON_API AMisinjeonPlayerController : public APlayerController
 {
     GENERATED_BODY()
 public:
-<<<<<<< HEAD
-	// HUD 위젯에 접근할 수 있는 함수 추가
-	UFUNCTION(BlueprintCallable, Category = "HUD")
-	UUserWidget* GetHUDWidget() const { return HUD; }
-	
-=======
+
     //UI 포커싱용
     UFUNCTION(BlueprintCallable, Category = "UI")
     void OpenUI(TSubclassOf<class UUserWidget> UIClass);
@@ -27,7 +22,6 @@ public:
     // HUD 위젯에 접근할 수 있는 함수
     UFUNCTION(BlueprintCallable, Category = "HUD")
     UUserWidget* GetHUDWidget() const { return HUD; }
->>>>>>> develop
 
 protected:
     // 게임이 시작될 때 HUD를 화면에 표시
@@ -50,26 +44,6 @@ private:
     UPROPERTY(EditAnywhere, Category = "HUD")
     TSubclassOf<UUserWidget> MainMenuWidgetClass;
 
-<<<<<<< HEAD
-	UPROPERTY(EditAnywhere, Category = "HUD")
-	TSubclassOf<UUserWidget> GameOverWidgetClass;
-
-	UPROPERTY(EditAnywhere, Category = "HUD")
-	TSubclassOf<UUserWidget> MainMenuWidgetClass;
-
-	// 생성된 HUD 위젯 인스턴스
-	UPROPERTY()
-	UUserWidget* HUD;
-
-	UPROPERTY()
-	UUserWidget* GameOverWidget;
-
-	// 생성된 MainMenu 위젯 인스턴스
-	UPROPERTY()
-	UUserWidget* MainMenuWidget;
-
-};
-=======
     // 생성된 HUD 위젯 인스턴스
     UPROPERTY()
     UUserWidget* HUD;
@@ -79,4 +53,4 @@ private:
     UPROPERTY()
     UUserWidget* MainMenuWidget;
 };
->>>>>>> develop
+
