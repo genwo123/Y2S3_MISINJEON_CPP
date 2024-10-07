975f0d54779923989763e08aad273b4d2e1f9303
Sonwonill <tlatmsdkqk@gmail.com>
2024-10-07 18:18:41 +0900

    :art:,:sparkles: Fix, Feat : "GameModeBase가 누락 되어서 재생성, "
    대화 시작시 HUD_Talking 표시

---
+++ Source/Y2S3_MISINJEON/Public/MiSinJeonGameModeBase.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiSinJeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class Y2S3_MISINJEON_API AMiSinJeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+
+	
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+
+};
